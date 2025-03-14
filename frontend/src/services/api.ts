d6c44584d9abc6e290fbefce6ecc1354793043e5
HMSagar2701 <sagarhm2701@gmail.com>
2025-03-14 09:56:15 +0530

    Changed from react to nextjs

--- frontend/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-    baseURL: 'https://sol-walk.onrender.com/api',
-  });
-
-export const registerUser = (data: any) => API.post('/auth/register', data);
-export const loginUser = (data: any) => API.post('/auth/login', data);
