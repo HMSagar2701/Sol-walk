666d356b67523733971fb2d65ee86804320fd003
HMSagar2701 <sagarhm2701@gmail.com>
2025-03-22 00:05:36 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,13 @@ app.set('trust proxy', 1); // Trust the first proxy (Render, Vercel, etc.)
 connectDB();
 
 // ✅ Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    credentials: true, // ✅ Allow cookies & headers
+  })
+);
+
 app.use(express.json());
 
 // ✅ Initialize Passport
